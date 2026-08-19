62e97265cdef9a2492228c2a516c9e16a21b5805
Mouhamed Sarr <mouhamed.sarr@dataventure.com>
2026-08-19 11:06:25 +0200

    refactor(ajouter): remove dead unreferenced module

--- src/app/features/ajouter/ajouter.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ajouter',
-  templateUrl: './ajouter.page.html',
-  styleUrls: ['./ajouter.page.scss'],
-  standalone: false,
-})
-export class AjouterPage {}
